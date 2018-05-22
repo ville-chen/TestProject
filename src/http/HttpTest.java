c4770601a8d7cb4f4daa02579d03cdc3f30d2620
ville <ville92.c@gmail.com>
2018-05-22 20:02:07 +0800

    增加 URLConnection Post请求json参数

---
+++ src/http/HttpTest.java
@@ -0,0 +1,80 @@
+package http;
+
+import com.alibaba.fastjson.JSON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * @author Ville
+ * @date 2018/5/22
+ */
+public class HttpTest {
+
+    /**
+     * 向指定 URL 发送POST方法的请求
+     *
+     * @param url   发送请求的 URL
+     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+     * @return 所代表远程资源的响应结果
+     */
+    public static String sendPost(String url, String param) {
+        OutputStreamWriter out = null;
+        BufferedReader in = null;
+        StringBuilder result = new StringBuilder();
+        try {
+            URL realUrl = new URL(url);
+            // 打开和URL之间的连接
+            HttpURLConnection conn = (HttpURLConnection) realUrl.openConnection();
+            // 设置通用的请求属性
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("connection", "keep-alive");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setUseCaches(false);
+            // 发送POST请求必须设置如下两行
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.connect();
+            // 获取URLConnection对象对应的输出流
+            out = new OutputStreamWriter(conn.getOutputStream(),"UTF-8");
+            // 发送请求参数
+            out.write(param);
+            // flush输出流的缓冲
+            out.flush();
+            // 定义BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result.append(line);
+            }
+            in.close();
+            conn.disconnect();
+        } catch (Exception e) {
+            System.out.println("发送 POST 请求出现异常: " + e.getMessage());
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        JsObj obj = new JsObj("client_credentials", "YXA6wDs-MARqEeSO0VcBzaqg5A", "YXA6JOMWlLap_YbI_ucz77j-4-mI0JA");
+        //发送 POST 请求
+        String sr = HttpTest.sendPost("https://a1.easemob.com/easemob-playground/test1/token",
+                JSON.toJSONString(obj));
+        System.out.println(sr);
+    }
+}
